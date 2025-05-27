24558335ab9b7b670e57fe10aa68501d29e86f7b
Kduxx <carlosedu.dev@gmail.com>
2025-05-27 20:58:00 -0300

    Chore: simplify api returns to a single response object

--- picpay_case/schemas/response.py
+++ picpay_case/schemas/response.py
@@ -1,23 +1,10 @@
 from pydantic import BaseModel, Field
 from typing import Generic, Optional, TypeVar
-from enum import Enum
 
 T = TypeVar('T')
 
 
-class ResponseStatus(str, Enum):
-    """Standardize the response types"""
-    SUCCESS = "success"
-    FAIL = "fail"
-    ERROR = "error"
-
-
 class APIResponse(BaseModel, Generic[T]):
-    status: ResponseStatus = Field(
-        default=None,
-        description="Response status of the api call"
-    )
-
     data: Optional[T] = Field(
         default={},
         description="The output data of an api call"
@@ -29,7 +16,7 @@ class APIResponse(BaseModel, Generic[T]):
     )
 
 
-class SuccessResponse(APIResponse[T]):
+class Response(APIResponse[T]):
 
     def __init__(
         self,
@@ -38,36 +25,8 @@ class SuccessResponse(APIResponse[T]):
         **kwargs
     ):
         super().__init__(
-                status=ResponseStatus.SUCCESS,
-                data=data,
-                message=message or "Operation successfull"
-        )
-
-
-class FailResponse(APIResponse[T]):
-
-    def __init__(
-        self,
-        message: Optional[str] = None,
-        **kwargs
-    ):
-        super().__init__(
-                status=ResponseStatus.FAIL,
-                message=message or "Unhandled error"
-        )
-
-
-class ErrorResponse(APIResponse[T]):
-    status: ResponseStatus = ResponseStatus.ERROR
-
-    def __init__(
-        self,
-        message: Optional[str] = None,
-        **kwargs
-    ):
-        super().__init__(
-                status=ResponseStatus.ERROR,
-                message=message or "Internal Server Error"
+            data=data,
+            message=message or "Operation successfull"
         )
 
 
@@ -76,18 +35,4 @@ def success_response(
     message: Optional[str] = None
 ) -> APIResponse[T]:
     """"""
-    return SuccessResponse(data=data if data else {}, message=message)
-
-
-def fail_response(
-    message: Optional[str] = "Unhandled error"
-) -> APIResponse[T]:
-    return FailResponse(message=message)
-
-
-def error_response(
-        data: Optional[T] = {},
-        message: Optional[str] = None
-) -> APIResponse[T]:
-    """"""
-    return ErrorResponse(data=data if data else {}, message=message)
+    return Response(data=data if data else {}, message=message)
